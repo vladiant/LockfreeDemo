6bc3eef2b515a9c88de284b7681f714d781049d8
Vladislav Antonov <vladislav.antonov@gmail.com>
2022-07-31 10:13:26 +0300

    repalce catch2 with utest

--- test/exchange_buffer_test.cpp
+++ test/exchange_buffer_test.cpp
@@ -1,6 +1,8 @@
 #include "lockfree/exchange_buffer.hpp"
 
-#include <catch2/catch.hpp>
+#include <utest/utest.h>
+
+#include <memory>
 
 namespace {
 
@@ -9,42 +11,52 @@ namespace {
 
 using IntBuffer = lockfree::ExchangeBuffer<int>;
 
-class TestExchangeBuffer {
+struct TestExchangeBuffer {
  public:
-  IntBuffer buffer;
+  std::unique_ptr<IntBuffer> buffer;
 };
+
+UTEST_F_SETUP(TestExchangeBuffer) {
+  static_cast<void>(utest_result);
+  utest_fixture->buffer = std::make_unique<IntBuffer>();
+}
+
+UTEST_F_TEARDOWN(TestExchangeBuffer) {
+  static_cast<void>(utest_result);
+  static_cast<void>(utest_fixture->buffer);
+}
 
 // Some tests for (single-threaded) correctness (necessary condition for
 // correctness).
 
-TEST_CASE_METHOD(TestExchangeBuffer, "constructed_buffer_is_empty") {
-  CHECK(buffer.empty());
+UTEST_F(TestExchangeBuffer, constructed_buffer_is_empty) {
+  EXPECT_TRUE(utest_fixture->buffer->empty());
 }
 
-TEST_CASE_METHOD(TestExchangeBuffer, "take_returns_nothing_if_empty") {
-  auto result = buffer.take();
-  CHECK_FALSE(result.has_value());
+UTEST_F(TestExchangeBuffer, take_returns_nothing_if_empty) {
+  auto result = utest_fixture->buffer->take();
+  EXPECT_FALSE(result.has_value());
 }
 
-TEST_CASE_METHOD(TestExchangeBuffer, "read_returns_nothing_if_empty") {
-  auto result = buffer.read();
-  CHECK_FALSE(result.has_value());
+UTEST_F(TestExchangeBuffer, read_returns_nothing_if_empty) {
+  auto result = utest_fixture->buffer->read();
+  EXPECT_FALSE(result.has_value());
 }
 
-TEST_CASE_METHOD(TestExchangeBuffer, "buffer_contains_value_after_write") {
-  CHECK(buffer.write(73));
-  CHECK_FALSE(buffer.empty());
-  auto result = buffer.take();
-  REQUIRE(result.has_value());
-  CHECK(*result == 73);
+UTEST_F(TestExchangeBuffer, buffer_contains_value_after_write) {
+  EXPECT_TRUE(utest_fixture->buffer->write(73));
+  EXPECT_FALSE(utest_fixture->buffer->empty());
+  auto result = utest_fixture->buffer->take();
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(*result, 73);
 }
 
-TEST_CASE_METHOD(TestExchangeBuffer, "write_overwrites_previous_value") {
-  CHECK(buffer.write(73));
-  CHECK(buffer.write(37));
-  auto result = buffer.take();
-  REQUIRE(result.has_value());
-  CHECK(*result == 37);
+UTEST_F(TestExchangeBuffer, write_overwrites_previous_value) {
+  EXPECT_TRUE(utest_fixture->buffer->write(73));
+  EXPECT_TRUE(utest_fixture->buffer->write(37));
+  auto result = utest_fixture->buffer->take();
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(*result, 37);
 }
 
 // We cannot easily test the case where a concurrent write fails
@@ -54,41 +66,41 @@ TEST_CASE_METHOD(TestExchangeBuffer, "write_overwrites_previous_value") {
 // Could be done by mocking the IndexPool but then we would have to expose
 // it as e.g. template parameter to allow mocking.
 
-TEST_CASE_METHOD(TestExchangeBuffer, "try_write_succeeds_if_empty") {
-  CHECK(buffer.try_write(73));
-  auto result = buffer.take();
-  REQUIRE(result.has_value());
-  CHECK(*result == 73);
+UTEST_F(TestExchangeBuffer, try_write_succeeds_if_empty) {
+  EXPECT_TRUE(utest_fixture->buffer->try_write(73));
+  auto result = utest_fixture->buffer->take();
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(*result, 73);
 }
 
-TEST_CASE_METHOD(TestExchangeBuffer, "try_write_fails_if_not_empty") {
-  CHECK(buffer.try_write(37));
-  CHECK_FALSE(buffer.empty());
-  CHECK_FALSE(buffer.try_write(73));
-  auto result = buffer.take();
-  REQUIRE(result.has_value());
-  CHECK(*result == 37);
+UTEST_F(TestExchangeBuffer, try_write_fails_if_not_empty) {
+  EXPECT_TRUE(utest_fixture->buffer->try_write(37));
+  EXPECT_FALSE(utest_fixture->buffer->empty());
+  EXPECT_FALSE(utest_fixture->buffer->try_write(73));
+  auto result = utest_fixture->buffer->take();
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(*result, 37);
 }
 
-TEST_CASE_METHOD(TestExchangeBuffer, "take_removes_value") {
-  CHECK(buffer.write(73));
-  auto result = buffer.take();
-  REQUIRE(result.has_value());
-  CHECK(*result == 73);
-  CHECK(buffer.empty());
-  result = buffer.take();
-  CHECK_FALSE(result.has_value());
+UTEST_F(TestExchangeBuffer, take_removes_value) {
+  EXPECT_TRUE(utest_fixture->buffer->write(73));
+  auto result = utest_fixture->buffer->take();
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(*result, 73);
+  EXPECT_TRUE(utest_fixture->buffer->empty());
+  result = utest_fixture->buffer->take();
+  EXPECT_FALSE(result.has_value());
 }
 
-TEST_CASE_METHOD(TestExchangeBuffer, "read_does_not_remove_value") {
-  CHECK(buffer.write(73));
-  auto result = buffer.read();
-  REQUIRE(result.has_value());
-  CHECK(*result == 73);
-  CHECK_FALSE(buffer.empty());
-  result = buffer.read();
-  REQUIRE(result.has_value());
-  CHECK(*result == 73);
+UTEST_F(TestExchangeBuffer, read_does_not_remove_value) {
+  EXPECT_TRUE(utest_fixture->buffer->write(73));
+  auto result = utest_fixture->buffer->read();
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(*result, 73);
+  EXPECT_FALSE(utest_fixture->buffer->empty());
+  result = utest_fixture->buffer->read();
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(*result, 73);
 }
 
 }  // namespace
